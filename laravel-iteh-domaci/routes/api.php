183a3713a2f1117b8389888e2bc4646d41cb5386
bane <bane.stojanovic00@gmail.com>
2021-12-12 23:08:31 +0100

    register function

--- laravel-iteh-domaci/routes/api.php
+++ laravel-iteh-domaci/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DriverController;
@@ -20,6 +21,8 @@ use App\Http\Controllers\VehicleController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/driver', [DriverController::class, 'index']);
 Route::get('/driver/{id}', [DriverController::class, 'show']);
